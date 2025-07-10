d4b894c72e6ce06303c659d95e52a5453f4bf54c
Filibert Heim <134500858+filibertmoritz@users.noreply.github.com>
2025-07-10 17:06:05 +0200

    Update Montserrat_forest_bird_monitoring_data_prep_Nmix_model_def_nimble.R
    Set samples and iter down for troubleshooting

--- Montserrat_forest_bird_monitoring_data_prep_Nmix_model_def_nimble.R
+++ Montserrat_forest_bird_monitoring_data_prep_Nmix_model_def_nimble.R
@@ -271,8 +271,8 @@ parameters.trend <- c("fit", "fit.new","trend","trend2","totalN","anndet","N")  
 
 # MCMC settings
 # number of posterior samples per chain is n.iter - n.burnin
-n.iter <- 250000 #150000
-n.burnin <- 100000  #100000
+n.iter <- 2500 #150000
+n.burnin <- 1000  #100000
 n.chains <- 3 #3
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
